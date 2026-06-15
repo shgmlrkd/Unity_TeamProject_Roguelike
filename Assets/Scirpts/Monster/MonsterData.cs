ef925dd802f0310ee90f6621ae6ec6ad499c5494
NamHyunjae <skaguswoxx66@gmail.com>
2026-06-15 16:34:48 +0900

    feat: Add MonsterData
    몬스터 데이터 이름, 프리펩, 최대체력, 이동속도, 데미지, 사거리, 공격딜레이 추가 했습니다.

---
+++ Assets/Scirpts/Monster/MonsterData.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "MonsterData", menuName = "GameData/Monster Data")]
+public class MonsterData : ScriptableObject
+{
+    [Header("몬스터 정보")]
+    [SerializeField] private string monsterName;
+    [SerializeField] private MonsterBase monsterPrefab;
+
+    [Header("몬스터 능력치")]
+    [Header("몬스터 체력")]
+    [SerializeField] private int monsterMaxHp;
+    [Header("몬스터 이동 속도")]
+    [SerializeField] private float moveSpeed;
+    [Header("몬스터 공격")]
+    [SerializeField] private int attackDamage;
+    [Header("공격 사거리")]
+    [SerializeField] private float attakcRange;
+    [Header("몬스터 공격 딜레이")]
+    [SerializeField] private float attackDelay;
+
+
+
+
+    public string MonsterName { get { return monsterName; } }
+    public MonsterBase MonsterPrefab { get { return monsterPrefab; } }
+    public int MonsterMaxHp { get { return monsterMaxHp; } }
+    public float MoveSpeed { get { return moveSpeed; } }
+    public int AttackDamage { get { return attackDamage; } }
+    public float AttakcRange { get { return attakcRange; } }
+    public float AttakcDelay { get { return attackDelay; } }
+
+
+}
