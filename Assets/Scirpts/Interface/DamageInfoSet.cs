c671578b645c4180b9befe59494de93907387c15
502lsh <ababababa334@gmail.com>
2026-06-16 16:15:23 +0900

    인터페이스, 데미지인포
    Add damageinfoset,interface IDamageable, IHealable

---
+++ Assets/Scirpts/Interface/DamageInfoSet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public struct DamageInfoSet
+{
+    public int damage;
+    public GameObject attacker;
+    public Vector2 hitVector;
+
+    public DamageInfoSet(
+        int damage,
+        GameObject attacker = null,
+        Vector2 hitVector = default)
+    {
+        this.damage = damage;
+        this.attacker = attacker;
+        this.hitVector = hitVector;
+    }
+}
