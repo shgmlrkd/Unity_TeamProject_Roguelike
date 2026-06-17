0b878a89f21102f93f56c1eb5f51cd5f9c7c1e96
jack <jack342@naver.com>
2026-06-17 15:17:46 +0900

    feat: add option system with volume and toggle settings
    마스터/BGM/SFX 볼륨 슬라이더를 enum으로 통합 관리했습니다.
    UI 텍스트 실시간 업데이트 및 옵션 표시 토글 기능을 추가 했습니다.

---
+++ Assets/Scirpts/UI/TitleUIController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TitleUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject optionPanel;
+
+    public void OnClickGameStart()
+    {
+        print("게임 시작");
+    }
+
+    public void OnClickEnterOption()
+    {
+        // 옵션 패널 활성화
+        optionPanel.SetActive(true);
+    }
+
+    public void OnClickExitOption()
+    {
+        // 여기서 GameManager에 볼륨과 표시 여부를 저장 시키면댐
+
+        // 옵션 패널 비활성화
+        optionPanel.SetActive(false);
+    }
+
+    public void OnClickExit()
+    {
+        print("게임 종료");
+    }
+}
---
+++ Assets/Scirpts/UI/UIButtonAnimation.cs
@@ -0,0 +1,64 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Vector3 originalScale;
+
+    [Header("마우스 오버 크기")]
+    [SerializeField]
+    private float hoverScale = 1.1f;
+
+    [Header("튕김 강도")]
+    [SerializeField]
+    private float punchPower = 0.1f;
+
+    [Header("시간")]
+    [SerializeField]
+    private float duration = 0.2f;
+
+    [SerializeField] 
+    private bool usePunch = false;
+
+    [SerializeField]
+    private bool useHover = true;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    // 클릭 시 호출되는 인터페이스 메서드
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // usePunch가 false면 클릭 시 애니메이션 적용 X
+        if (!usePunch) return;
+
+        // 클릭 시 애니메이션 효과를 적용함
+        transform.DOKill();
+        transform.DOPunchScale(originalScale * punchPower, duration);
+    }
+
+    // 마우스 포인터가 버튼에 오버랩 될 때 호출되는 인터페이스 메서드
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // useHover가 false면 마우스가 오버랩 시 애니메이션 적용 X
+        if (!useHover) return;
+
+        // 오버랩 시 애니메이션 효과를 적용함
+        transform.DOKill();
+        transform.DOScale(Vector3.one * hoverScale, duration).SetEase(Ease.OutQuad);
+    }
+
+    // 마우스 포인터가 버튼에 오버랩되어 있다가 빠져나올 때 호출되는 인터페이스 메서드
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // useHover가 false면 아래 코드는 의미가 없으므로 애니메이션 적용 X
+        if (!useHover) return;
+
+        // 마우스가 벗어날 때 애니메이션 효과를 적용함
+        transform.DOKill();
+        transform.DOScale(originalScale, duration).SetEase(Ease.OutQuad);
+    }
+}
