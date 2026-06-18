898bd5632522c6b78ae674651ec48b9b4bc25e46
502lsh <ababababa334@gmail.com>
2026-06-18 17:53:37 +0900

    Add HeartUI, PlayerHP Split CharacterController2D to PlayerWeaponController, PlayerAttackController
    플레이어 스크립트 테스트 완료 PlayerHP를 기반으로 화면의 UI가 변경하도록 씬에 배치
    CharacterController2D 너무 많은 역할 PlayerAttackController,PlayerWeaponController 로 역할 분리

---
+++ Assets/Scirpts/Player/PlayerHP.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour, IDamageable
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private int maxHp = 10;
+    private int currentHp;
+
+    private bool isDead;
+
+    public int MaxHp => maxHp;
+    public int CurrentHp => currentHp;
+    public bool IsDead => isDead;
+
+    public event Action<int, int> OnHpChanged;
+    //현재 체력 갱신 테스트
+    //[Header("Test")]
+    //[SerializeField] private int testCurrentHp = -1;
+
+    //private int previousTestCurrentHp = -1;
+
+    private void Awake()
+    {
+        currentHp = maxHp;
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+    private void Start()
+    {
+        OnHpChanged?.Invoke(currentHp, maxHp);
+    }
+
+    //private void Update()
+    //{
+    //    UpdateTestCurrentHp();
+    //}
+    public void TakeDamage(DamageInfoSet damageInfoset) // 받는 공격 데미지
+    {
+        if (isDead)
+        {
+            return;
+        }
+        // DamageInfoSet 의
+        currentHp -= damageInfoset.Damage;
+        GameObject attacker = damageInfoset.Attacker;
+        Vector2 Direction = damageInfoset.AttackDirection;
+        currentHp = Mathf.Clamp(currentHp, 0, maxHp);
+
+        OnHpChanged?.Invoke(currentHp, maxHp);
+
+        if (currentHp <= 0)
+        {
+            currentHp = 0;
+            
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+        }
+        else
+        {
+            //애니메이터 오류발생대응
+            Destroy(gameObject);
+        }
+    }
+    //사망 애니메이션 마지막 프레임에 호출
+    public void DestroyPlayer()
+    {
+        Destroy(gameObject);
+    }
+    //private void UpdateTestCurrentHp()
+    //{
+    //    if (testCurrentHp == previousTestCurrentHp)
+    //    {
+    //        return;
+    //    }
+
+    //    previousTestCurrentHp = testCurrentHp;
+
+    //    if (testCurrentHp < 0)
+    //    {
+    //        return;
+    //    }
+
+    //    currentHp = Mathf.Clamp(testCurrentHp, 0, maxHp);
+    //    isDead = currentHp <= 0;
+
+    //    OnHpChanged?.Invoke(currentHp, maxHp);
+    //}
+}
---
+++ Assets/Scirpts/Player/PlayerWeaponController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    [Header("Weapon")]
+    [SerializeField] private WeaponData defaultWeapon;
+    [SerializeField] private WeaponData currentWeapon;
+    [SerializeField] private float weaponDropDistance = 0.8f;
+    [SerializeField] private float droppedWeaponPickupDelay = 2.0f;
+
+    private CharacterController2D characterController;
+
+    public WeaponData CurrentWeapon => currentWeapon;
+
+    public event Action<WeaponData> OnWeaponChanged;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController2D>();
+
+        if (currentWeapon == null)
+        {
+            currentWeapon = defaultWeapon;
+        }
+    }
+
+    private void Start()
+    {
+        OnWeaponChanged?.Invoke(currentWeapon);
+    }
+    //들고 있던 무기 떨어트리고 장착
+    public void EquipWeapon(WeaponData newWeapon)
+    {
+        if (newWeapon == null)
+        {
+            return;
+        }
+
+        DropCurrentWeapon();
+
+        currentWeapon = newWeapon;
+        OnWeaponChanged?.Invoke(currentWeapon);
+
+        Debug.Log($"무기 장착: {currentWeapon.WeaponName}");
+    }
+    //무기 떨어트리는 위치 제어
+    private void DropCurrentWeapon()
+    {
+        if (currentWeapon == null || currentWeapon.WeaponItemPrefab == null)
+        {
+            return;
+        }
+
+        Vector2 lookDirection = Vector2.down;
+
+        if (characterController != null)
+        {
+            lookDirection = characterController.LookDirection;
+        }
+
+        Vector2 dropDirection = -lookDirection;
+
+        if (dropDirection == Vector2.zero)
+        {
+            dropDirection = Vector2.down;
+        }
+
+        Vector3 dropPosition = transform.position + (Vector3)(dropDirection.normalized * weaponDropDistance);
+
+        GameObject droppedWeapon = Instantiate(
+            currentWeapon.WeaponItemPrefab,
+            dropPosition,
+            Quaternion.identity
+        );
+
+        if (droppedWeapon.TryGetComponent(out WeaponItem weaponItem))
+        {
+            weaponItem.SetPickupDelay(droppedWeaponPickupDelay);
+        }
+    }
+}
