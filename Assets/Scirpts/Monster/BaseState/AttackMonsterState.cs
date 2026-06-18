fb5a4c51406aebe6e9cd2617409c3815b4305454
NamHyunjae <skaguswoxx66@gmail.com>
2026-06-18 10:33:27 +0900

    feat : Add ChaseMonsterState, AttackMonsterState, MonsterHp/ refactor : Refactor patrolState,
    몬스터 추적기능과 몬스터 HP(데미지 받기)를 새로 추가했으며 리지드바디 추가로 코드를 수정했습니다.

---
+++ Assets/Scirpts/Monster/BaseState/AttackMonsterState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackMonsterState : MonsterBase
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    public void AttackDamage()
+    {
+        if (monsterStateManager.Target == null) return;
+
+        if (monsterStateManager.Target.TryGetComponent(out IDamageable damageable))
+        {
+            DamageInfoSet damageInfoSet = new DamageInfoSet(); // 구조체에 있는 방식으로 전달
+            damageInfoSet.Damage = monsterStateManager.MonsterData.AttackDamage;
+            damageInfoSet.Attacker = gameObject;
+
+            damageable.TakeDamage(damageInfoSet);
+            print("공격중");
+        }
+    }
+    // 애니메이션 프레임으로 이벤트 추가로 공격이 되게 연결 할 예정
+    // 코루틴으로 안해도 충분하다
+}
---
+++ Assets/Scirpts/Monster/BaseState/DeadMonsterState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DeadMonsterState : MonsterBase
+{
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        
+    }
+
+    private void OnEnable()
+    {
+        DeadAnimation();
+    }
+
+    private void DeadAnimation()
+    {
+        rb.linearVelocity = Vector2.zero; // 죽었을 때 움직임 멈추기
+        monsterCollider2D.enabled = false; // 충돌 끄기
+
+
+    }
+
+}
