8e72e9f441ea39f2c47dec087e9f85f5aa05cd5e
jack <jack342@naver.com>
2026-06-19 14:03:40 +0900

    Refactor OptionUIAnimation to separate show/hide animation logic
    옵션 패널의 Show/Hide 애니메이션 로직을 OptionUIAnimation으로 UI 애니메이션을 분리했습니다.

---
+++ Assets/Scirpts/UI/Animation/OptionUIAnimation.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class OptionUIAnimation : MonoBehaviour
+{
+    [Header("옵션 패널")]
+    [SerializeField] 
+    private Transform target;
+
+    [Header("옵션 캔버스 그룹")]
+    [SerializeField] 
+    private CanvasGroup canvasGroup;
+
+    public void Show()
+    {
+        // 옵션 패널 활성화
+        target.gameObject.SetActive(true);
+
+        // 애니메이션 재생 중 입력 비활성화
+        canvasGroup.blocksRaycasts = false;
+
+        Sequence sequence = DOTween.Sequence();
+
+        // 옵션 패널 열기 애니메이션 재생
+        sequence.Append(UIAnimationUtility.ShowScale(target,
+            UIAnimationSettings.NormalDuration));
+
+        // 애니메이션이 일정 비율 진행되면 입력 활성화
+        sequence.InsertCallback(UIAnimationSettings.CallbackRatio, () =>
+        {
+            canvasGroup.blocksRaycasts = true;
+        });
+    }
+
+    public void Hide()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        // 옵션 패널 닫기 애니메이션 재생
+        sequence.Append(UIAnimationUtility.HideScale(target,
+            UIAnimationSettings.NormalDuration));
+
+        // 애니메이션이 일정 비율 진행되면 입력 비활성화
+        sequence.InsertCallback(UIAnimationSettings.CallbackRatio, () =>
+        {
+            canvasGroup.blocksRaycasts = false;
+        });
+
+        // 애니메이션 종료 후 패널 비활성화
+        sequence.OnComplete(() =>
+        {
+            target.gameObject.SetActive(false);
+        });
+    }
+}
