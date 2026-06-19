560c167bf1746a5cb94c86959dc02b1cfaae9a2a
jack <jack342@naver.com>
2026-06-19 20:52:09 +0900

    feat: Add bonus health support to equipment items
    - 아이템 데이터에 추가 체력 옵션 추가
    - 장비 데이터 설정 업데이트
    - 추가 체력 UI용 하트 이미지 추가

--- Assets/Scirpts/Data/Item/EquipmentData.cs
+++ Assets/Scirpts/Data/Item/EquipmentData.cs
@@ -18,9 +18,13 @@ public class EquipmentData : ItemData
     [SerializeField]
     private float moveSpeedRate;
 
-    public EquipmentType EquipmentType => equipmentType; // 장비 타입
-    public int Attack => attack;                        // 공격력
-    public float AttackRange => attackRange;            // 공격 범위
-    public float AttackSpeedRate => attackSpeedRate;    // 공격 속도 계수 (애니메이션 용도)
-    public float MoveSpeedRate => moveSpeedRate;        // 이동 속도 계수
+    [SerializeField]
+    private int shieldHp;
+
+    public EquipmentType EquipmentType => equipmentType;    // 장비 타입
+    public int Attack => attack;                            // 공격력
+    public float AttackRange => attackRange;                // 공격 범위
+    public float AttackSpeedRate => attackSpeedRate;        // 공격 속도 계수 (애니메이션 용도)
+    public float MoveSpeedRate => moveSpeedRate;            // 이동 속도 계수
+    public int ShieldHp => shieldHp;                        // 추가 체력
 }
