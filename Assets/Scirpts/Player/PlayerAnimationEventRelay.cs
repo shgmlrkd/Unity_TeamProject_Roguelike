ad13c685d4bbf52b29c1571319b21f267a082f7e
502lsh <ababababa334@gmail.com>
2026-06-22 15:52:29 +0900

    Add Player animation repair Playerhp, HeartUI, CharacterController2d,
    플레이어 애니메이션 추가 hp오류 수정

---
+++ Assets/Scirpts/Player/PlayerAnimationEventRelay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationEventRelay : MonoBehaviour
+{
+    //플레이어와 플레이어 루트 중계용 스크립트
+
+    [SerializeField] private PlayerAttackController attackController;
+    [SerializeField] private PlayerHP playerHp;
+
+    private void Awake()
+    {
+        if (attackController == null)
+        {
+            attackController = GetComponentInParent<PlayerAttackController>();
+        }
+
+        if (playerHp == null)
+        {
+            playerHp = GetComponentInParent<PlayerHP>();
+        }
+    }
+
+    public void EnableAttackCollider()
+    {
+        attackController?.EnableAttackCollider();
+    }
+
+    public void DisableAttackCollider()
+    {
+        attackController?.DisableAttackCollider();
+    }
+
+    public void EndAttack()
+    {
+        attackController?.EndAttack();
+    }
+
+    public void DestroyPlayer()
+    {
+        playerHp?.DestroyPlayer();
+    }
+}
