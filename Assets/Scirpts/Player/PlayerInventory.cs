8200b6549e1aa92c5cdf539d7f20dde99e249d71
502lsh <ababababa334@gmail.com>
2026-06-22 22:38:06 +0900

    feats Add PlayerInventoryPrimary
    어택 콜라이더 고정 인벤토리 기본(저장만) 장착 탈착 따로 만들거나 기존 스크립트에 추가

---
+++ Assets/Scirpts/Player/PlayerInventory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private Dictionary<EquipmentType, EquipmentData> equipmentItems =
+        new Dictionary<EquipmentType, EquipmentData>();
+
+    public event Action<EquipmentType, EquipmentData> OnEquipmentStored;
+    public event Action<EquipmentType> OnEquipmentRemoved;
+
+    public void StoreEquipment(EquipmentData equipmentData)
+    {
+        if (equipmentData == null)
+        {
+            return;
+        }
+
+        EquipmentType equipmentType = equipmentData.EquipmentType;
+
+        equipmentItems[equipmentType] = equipmentData;
+
+        OnEquipmentStored?.Invoke(equipmentType, equipmentData);
+
+        //Debug.Log($"장비 저장: {equipmentType} / {equipmentData.ItemName}");
+    }
+
+    public bool TryGetEquipment(EquipmentType equipmentType, out EquipmentData equipmentData)
+    {
+        return equipmentItems.TryGetValue(equipmentType, out equipmentData);
+    }
+
+    public bool HasEquipment(EquipmentType equipmentType)
+    {
+        return equipmentItems.ContainsKey(equipmentType);
+    }
+
+    public void RemoveEquipment(EquipmentType equipmentType)
+    {
+        if (!equipmentItems.ContainsKey(equipmentType))
+        {
+            return;
+        }
+
+        equipmentItems.Remove(equipmentType);
+
+        OnEquipmentRemoved?.Invoke(equipmentType);
+
+        //Debug.Log($"장비 제거: {equipmentType}");
+    }
+
+    public void Clear()
+    {
+        equipmentItems.Clear();
+    }
+}
