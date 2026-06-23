0b978fd4c3d1b3caf35a759ffa2f83388cd65d46
NamHyunjae <skaguswoxx66@gmail.com>
2026-06-23 19:31:19 +0900

    feat : Add AnimationController
    몬스터 애니메이션 추가했습니다.

---
+++ Assets/Scirpts/Monster/Animation/AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator refAnimator;
+
+
+    private void Awake()
+    {
+        if (refAnimator == null)
+        {
+            refAnimator = GetComponent<Animator>();
+        }
+    }
+
+    public void OnStateChanged(MonsterStateEnum monsterState)
+    {
+        if (refAnimator == null) return;
+
+        refAnimator.SetInteger("MonsterState", (int)monsterState);
+    }
+
+    public void SetBossMonsterPattern(BossMonsterPattern bossMonsterPattern)
+    {
+        if (refAnimator == null) return;
+
+        refAnimator.SetInteger("BossMonster", (int)bossMonsterPattern);
+    }
+
+    public AnimatorStateInfo GetcurrentStateInfo()
+    {
+        return refAnimator.GetCurrentAnimatorStateInfo(0);
+    }
+
+}
