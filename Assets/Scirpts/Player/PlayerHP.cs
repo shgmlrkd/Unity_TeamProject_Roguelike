8085d20eacadff5e8631aaef28e2abff624e2faa
502lsh <ababababa334@gmail.com>
2026-06-24 15:57:31 +0900

    Add DeathFade improve code
    Die 애니메이션 동안 입력 불가, 공격중 플립 불가하게 수정 게임오버시 점차 스프라이트 옅어짐 추가

--- Assets/Scirpts/Player/PlayerHP.cs
+++ Assets/Scirpts/Player/PlayerHP.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerHP : MonoBehaviour, IDamageable
 {
@@ -13,6 +11,15 @@ public class PlayerHP : MonoBehaviour, IDamageable
 
 
     [SerializeField] private float invincibleTime = 0.8f;
+
+    [Header("Death Fade")]
+    [SerializeField] private float fadeOutDuration = 1.0f;
+    [SerializeField] private float disableAlphaThreshold = 0.02f;
+
+    private SpriteRenderer[] spriteRenderers;
+    private Color[] originalSpriteColors;
+    private Coroutine fadeOutCoroutine;
+
     private int currentHp;
     // 추가 체력 저장할 변수
     private int currentBonusHp = 0;
@@ -25,6 +32,10 @@ public class PlayerHP : MonoBehaviour, IDamageable
     private PlayerInventory playerInventory;
     private int previousEquipmentShieldHp;
 
+    private CharacterController2D characterController;
+    private PlayerAttackController attackController;
+    private CharacterInputManager inputManager;
+    private Rigidbody2D rb;
     public int MaxHp => maxHp;
     public int CurrentHp => currentHp;
     public int CurrentBonusHp => currentBonusHp;
@@ -47,7 +58,19 @@ public class PlayerHP : MonoBehaviour, IDamageable
             animator = GetComponentInChildren<Animator>();
         }
 
+        characterController = GetComponent<CharacterController2D>();
+        attackController = GetComponent<PlayerAttackController>();
+        inputManager = GetComponent<CharacterInputManager>();
+        rb = GetComponent<Rigidbody2D>();
         playerInventory = GetComponent<PlayerInventory>();
+
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>(true);
+        originalSpriteColors = new Color[spriteRenderers.Length];
+
+        for (int i = 0; i < spriteRenderers.Length; i++)
+        {
+            originalSpriteColors[i] = spriteRenderers[i].color;
+        }
     }
 
     private void Start()
@@ -171,6 +194,8 @@ public class PlayerHP : MonoBehaviour, IDamageable
     {
         isDead = true;
 
+        DisableControl();
+        
         if (animator != null)
         {
             animator.SetTrigger("Die");
@@ -178,7 +203,7 @@ public class PlayerHP : MonoBehaviour, IDamageable
         else
         {
             //애니메이터 오류발생대응
-            Destroy(gameObject);
+            StartFadeOut();
         }
     }
     //피격시 무적 설정
@@ -203,7 +228,93 @@ public class PlayerHP : MonoBehaviour, IDamageable
     //사망 애니메이션 마지막 프레임에 호출
     public void DestroyPlayer()
     {
-        Destroy(gameObject);
+        StartFadeOut();
+    }
+    private void StartFadeOut()
+    {
+        if (fadeOutCoroutine != null)
+        {
+            return;
+        }
+
+        fadeOutCoroutine = StartCoroutine(FadeOutAndDisableCo());
+    }
+    private IEnumerator FadeOutAndDisableCo()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            DisablePlayer();
+            yield break;
+        }
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeOutDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp01(elapsedTime / fadeOutDuration);
+            float alphaMultiplier = Mathf.Lerp(1f, 0f, t);
+
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                if (spriteRenderers[i] == null)
+                {
+                    continue;
+                }
+
+                Color color = originalSpriteColors[i];
+                color.a = originalSpriteColors[i].a * alphaMultiplier;
+                spriteRenderers[i].color = color;
+            }
+
+            if (alphaMultiplier <= disableAlphaThreshold)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+        for (int i = 0; i < spriteRenderers.Length; i++)
+        {
+            if (spriteRenderers[i] == null)
+            {
+                continue;
+            }
+
+            Color color = spriteRenderers[i].color;
+            color.a = 0f;
+            spriteRenderers[i].color = color;
+        }
+
+        DisablePlayer();
+    }
+    private void DisablePlayer()
+    {
+        gameObject.SetActive(false);
+    }
+    private void DisableControl()
+    {
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        if (characterController != null)
+        {
+            characterController.enabled = false;
+        }
+
+        if (attackController != null)
+        {
+            attackController.enabled = false;
+        }
+
+        if (inputManager != null)
+        {
+            inputManager.enabled = false;
+        }
     }
     //private void UpdateTestCurrentHp()
     //{
