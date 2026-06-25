10d137b1c5ba2fc631627649dbe0dd017b614c9a
502lsh <ababababa334@gmail.com>
2026-06-25 17:11:10 +0900

    Add SlashAttackVFX, Add RandomStat
    VFX 추가 랜덤 스탯시스템 생성 및 스탯에 적용 쉴드 체력 적용 방식 수정

---
+++ Assets/Scirpts/Player/RollStat/PlayerStartStatBonus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerStartStatBonus : MonoBehaviour
+{
+    private PlayerRolledStat startStat;
+
+    public int Attack => startStat != null ? startStat.Attack : 0;
+    public float AttackSpeedRate => startStat != null ? startStat.AttackSpeedRate : 0f;
+    public float MoveSpeedRate => startStat != null ? startStat.MoveSpeedRate : 0f;
+    public int ShieldHp => startStat != null ? startStat.ShieldHp : 0;
+
+    public void SetStartBonusStat(PlayerRolledStat stat)
+    {
+        startStat = stat;
+    }
+}
---
+++ Assets/Scirpts/Player/RollStat/StartStatRoller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartStatRoller : MonoBehaviour
+{//시작 버튼에 연결 필요
+    //랜덤 범위 지정
+    [Header("HP Range")]
+    [SerializeField] private int minMaxHp = 1;
+    [SerializeField] private int maxMaxHp = 5;
+
+    [Header("Attack Range")]
+    [SerializeField] private int minAttack = 1;
+    [SerializeField] private int maxAttack = 3;
+
+    [Header("Attack Speed Rate Range")]
+    [SerializeField] private float minAttackSpeedRate = -0.2f;
+    [SerializeField] private float maxAttackSpeedRate = 0.3f;
+
+    [Header("Move Speed Rate Range")]
+    [SerializeField] private float minMoveSpeedRate = -0.2f;
+    [SerializeField] private float maxMoveSpeedRate = 0.3f;
+
+    [Header("Shield HP Range")]
+    [SerializeField] private int minShieldHp = 0;
+    [SerializeField] private int maxShieldHp = 2;
+
+    public void RollStats()
+    {
+        PlayerRolledStat rolledStat = new PlayerRolledStat
+        {
+            //랜덤 돌림
+            MaxHp = 2*(Random.Range(minMaxHp, maxMaxHp + 1)),
+            Attack = Random.Range(minAttack, maxAttack + 1),
+            AttackSpeedRate = Random.Range(minAttackSpeedRate, maxAttackSpeedRate),
+            MoveSpeedRate = Random.Range(minMoveSpeedRate, maxMoveSpeedRate),
+            ShieldHp = 2*(Random.Range(minShieldHp, maxShieldHp + 1))
+        };
+
+        PlayerStartStatStorage.SetRolledStat(rolledStat);
+
+    }
+}
