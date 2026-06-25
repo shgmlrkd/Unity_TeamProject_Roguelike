22b920bddb3552c63c2b1b61b0f93b5f2b7faa0e
NamHyunjae <skaguswoxx66@gmail.com>
2026-06-25 20:01:21 +0900

    refactor : Refactor MonsterData / docs : Update PatrolMonsterState
    몬스터 데이터에 드랍 아이템수 추가했습니다.
    
    패트롤 상태 스크립트에 프린트, 디버그로그 들을 주석 처리 했습니다

--- Assets/Scirpts/Monster/BaseState/PatrolMonsterState.cs
+++ Assets/Scirpts/Monster/BaseState/PatrolMonsterState.cs
@@ -40,15 +40,15 @@ public class PatrolMonsterState : MonsterBase
     private IEnumerator PatrolCo()  // 랜덤 위치 순찰
     {
         bool hasPath = TrySetRandomPath(); // 랜덤 순찰 목표까지의 경로 생성
-        print($"경로 생성완료 : {hasPath}");
+        //print($"경로 생성완료 : {hasPath}");
         if (hasPath) // 목표 경로가 있다면 이동
         {
             yield return MovePath();
-            print("패트롤 끝");
+            //print("패트롤 끝");
         }
         else
         {
-            print("경로 못받음");
+           // print("경로 못받음");
         }
         monsterStateManager.SetState(MonsterStateEnum.Idle);
 
@@ -60,23 +60,23 @@ public class PatrolMonsterState : MonsterBase
     {
         while ( currentPath != null && pathIndex < currentPath.Count) // 경로가 존재하며 목적지에 도착하지 않았다면
         {
-            print("현재 목표 노드의 월드 좌표 가져오기 시작");
+            //print("현재 목표 노드의 월드 좌표 가져오기 시작");
             Vector3 targetPos = pathFinder.Grid.GetWorldPosition(currentPath[pathIndex]); // 현재 목표 노드의 월드 좌표 가져오기
-            print($"현재 목표 노드의 월드 좌표 옴 {targetPos}");
+            //print($"현재 목표 노드의 월드 좌표 옴 {targetPos}");
             Vector2 nextPosition = Vector2.MoveTowards(
                 rb.position,
                 targetPos,
                 monsterStateManager.MonsterData.PatrolSpeed * Time.fixedDeltaTime); // 목표까지 이동
-            print($"다음 경로 {nextPosition}");
+            //print($"다음 경로 {nextPosition}");
             rb.MovePosition(nextPosition);
 
             monsterStateManager.FlipTo(targetPos);
 
             if (Vector2.Distance(nextPosition, targetPos) < 0.15f) // 목표 노드에 도착했다면 다음 목표 노드로 변경 도착 판정을 널널하게
             {
-                print($"도착 {pathIndex}");
+                //print($"도착 {pathIndex}");
                 pathIndex++;
-                print($"인덱스 변경 {pathIndex}");
+                //print($"인덱스 변경 {pathIndex}");
             }
 
             yield return new WaitForFixedUpdate();
@@ -102,14 +102,14 @@ public class PatrolMonsterState : MonsterBase
 
         if(randomNode == null)
         {
-            print("랜덤노느 못가져옴");
+            //print("랜덤노느 못가져옴");
             return false;
         }
         
         // 랜덤으로 뽑은 노드를 월드 좌표로 전환
         Vector3 targetPos = pathFinder.Grid.GetWorldPosition(randomNode);
-        print($"현재 경로 변수 : {currentPath}");
-        print($"목표 좌표 : {targetPos}");
+        //print($"현재 경로 변수 : {currentPath}");
+        //print($"목표 좌표 : {targetPos}");
         // 현재 위치에서 랜덤 목표 위치까지 경로 생성
         currentPath = pathFinder.FindPath(rb.position, targetPos);
 
@@ -117,7 +117,7 @@ public class PatrolMonsterState : MonsterBase
         // 현재 위치만 들어있는 경로면 실패
         if (currentPath == null || currentPath.Count <= 1)
         {
-            print("경로가 없거나 현재위치만 있음");
+            //print("경로가 없거나 현재위치만 있음");
             return false;
         }
 
