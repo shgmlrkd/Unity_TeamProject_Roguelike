ff3766f0b63efa4ccdffb8146d0490267b9d9958
hoy467 <hoyhoy4678@gmail.com>
2026-06-26 21:47:13 +0900

    assets:Remove
    enum추가, 보스방 겹치 버그 수정
    맵카운트 이후 맵생성 막음 텔포버그 수정
    보스입장방 1개 추가

--- Assets/Scirpts/Maps/NewRoom/RoomGenerator1.cs
+++ Assets/Scirpts/Maps/NewRoom/RoomGenerator1.cs
@@ -5,12 +5,17 @@ public class RoomGenerator1 : MonoBehaviour
     // 방의 정보와 위치, 그리고 입장 방향을 받아 실제 게임 오브젝트로 생성합니다.
     public GameObject CreateRoom(RoomInfo roomInfo, Vector3 worldPos, Vector2Int gridPos, Vector2Int doorDirection)
     {
+        if (!RoomRuleChecker1.Instance.CanGenerateMoreRooms && roomInfo.type != RoomType.Boss) return null;
+        if (roomInfo.type == RoomType.Boss && !RoomRuleChecker1.Instance.CanGenerateMoreRooms) return null;
+
         GameObject roomObj = Instantiate(roomInfo.roomPrefab, worldPos, Quaternion.identity);
 
+        if (roomInfo.type == RoomType.Boss)
+            RoomRuleChecker1.Instance.CanGenerateMoreRooms = false;
+
         if (roomObj.TryGetComponent<DynamicRoom>(out var comp))
-        {
             comp.SetupRoom(gridPos, roomInfo);
-        }
+
         return roomObj;
     }
     // 연결 방향 정보가 필요 없는 경우(시작 방 등)를 위한 오버로딩 함수
