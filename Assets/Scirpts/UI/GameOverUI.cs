3bd20eb2ae718a7e9d6241745e08f81f73bd06b4
jack <jack342@naver.com>
2026-06-27 17:11:39 +0900

    refactor : InGameManager, GameOverUi, InGameTimer, InGameUIController
    InGameUIController 스크립트에 게임오버 이벤트 등록 추가
    InGameManager에서 역할에 어긋나는 GameOverUI 필드 제거
    GameOverUI 텍스트, 버튼 기능 추가
    InGameTimer에 게임오버 시 타이머 정지 추가

--- Assets/Scirpts/UI/GameOverUI.cs
+++ Assets/Scirpts/UI/GameOverUI.cs
@@ -4,26 +4,46 @@ using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    // 1. 플레이 타임
-    // 2. 보스 처치 유무
-    // 3. 몬스터 처치 수
-
     [SerializeField]
     private TextMeshProUGUI[] gameOverTexts;
-
-    // 내일 이어서 버튼 구현 하기
+ 
     [SerializeField]
     private Button[] gameOverButtons;
 
     public void Result()
     {
+        // 플레이 타임
         gameOverTexts[(int)GameOverTextType.PlayTime].text =
                                 InGameManager.Instance.GameOverTime;
         
+        // 보스 처치 유무
         gameOverTexts[(int)GameOverTextType.BossClearCheck].text =
                                 InGameManager.Instance.GetBossKillResult();
 
+        // 몬스터 처치 수
         gameOverTexts[(int)GameOverTextType.MonsterKillCount].text =
                                 InGameManager.Instance.GetMonsterKillResult();
+    }
+
+    // 다시 하기
+    public void OnClickRetry()
+    {
+        GameSceneManager.Instance.ReloadCurScene();
+    }
+
+    // 메인 메뉴
+    public void OnClickMainMenu()
+    {
+        GameSceneManager.Instance.LoadScene(SceneType.Title);
+    }
+
+    // 게임 종료
+    public void OnClickQuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
 }
