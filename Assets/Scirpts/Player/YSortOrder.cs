22fa8f63371322d0d9474787e086ef1849a9e4ae
502lsh <ababababa334@gmail.com>
2026-06-29 13:56:57 +0900

    Add YSortingLayerOrder
    y축기준 솔팅레이어 통제 스크립트? 추가

---
+++ Assets/Scirpts/Player/YSortOrder.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class YSortOrder : MonoBehaviour
+{
+    [SerializeField] private SortingGroup sortingGroup;
+    [SerializeField] private float sortingScale = 100f;
+    [SerializeField] private int sortingOffset = 0;
+
+    private int previousSortingOrder;
+
+    private void Awake()
+    {
+        if (sortingGroup == null)
+        {
+            sortingGroup = GetComponent<SortingGroup>();
+        }
+
+        UpdateSortingOrder(true);
+    }
+
+    private void LateUpdate()
+    {
+        UpdateSortingOrder(false);
+    }
+
+    private void UpdateSortingOrder(bool forceUpdate)
+    {
+        if (sortingGroup == null)
+        {
+            return;
+        }
+        //y값에 따른 Sorting Layer 통제
+        int sortingOrder = Mathf.RoundToInt(-transform.position.y * sortingScale) + sortingOffset;
+
+        if (!forceUpdate && previousSortingOrder == sortingOrder)
+        {
+            return;
+        }
+
+        previousSortingOrder = sortingOrder;
+        sortingGroup.sortingOrder = sortingOrder;
+    }
+}
