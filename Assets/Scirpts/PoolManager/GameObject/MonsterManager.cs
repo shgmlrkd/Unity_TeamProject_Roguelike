f655b238ddd0b3eb4ef36c224d65d7c199cb051e
NamHyunjae <skaguswoxx66@gmail.com>
2026-06-29 13:21:16 +0900

    fix : Fix AttackMonsterState, RangedAttackMonsterState / faet : Add MonsterBullet , SkeletonBullet, MagicBullet/ Updated MonsterManager
    Fix
    몬스터 이동하면서 공격하는걸 수정했습니다.
    
    ------
    Add
    MonsterBullet  파일 이동및  총알 스크립트 추가했습니다.
    SkeletonBullet, MagicBullet 풀 타입을 위해 빈 스크립트 작성했습니다.
    
    ---
    Updated
    MonsterManager 투사체 오브젝트 추가로 몬스터 매니저 에 프리팹 공간 추가 및 생성 코드 추가했습니다.

--- Assets/Scirpts/PoolManager/GameObject/MonsterManager.cs
+++ Assets/Scirpts/PoolManager/GameObject/MonsterManager.cs
@@ -5,6 +5,15 @@ public class MonsterManager : ScenesSingleton<MonsterManager>
     [Header("프리팹")]
     [SerializeField] private MonsterStateManager[] monsterPrefabs;
 
+
+    [Header("화살 프리팹")]
+    [SerializeField] private SkeletonBullet[] skeletonBulletPergabs;
+
+    [Header("매직볼 프리팹")]
+    [SerializeField] private MagicBullet[] monsterBulletPrefabs;
+
+
+
     private int poolSize = 10;
 
     private bool isAllMonsterDead = false;
@@ -18,6 +27,17 @@ public class MonsterManager : ScenesSingleton<MonsterManager>
         {
             PoolManager.Instance.PreloadPool(monsterPrefabs[i], poolSize); // 몬스터 프리펩들을 미리 풀에 만들기
         }
+
+        for (int i = 0; i < skeletonBulletPergabs.Length; ++i)
+        {
+            PoolManager.Instance.PreloadPool(skeletonBulletPergabs[i], poolSize);
+        }
+
+        for (int i = 0; i < monsterBulletPrefabs.Length; ++i)
+        {
+            PoolManager.Instance.PreloadPool(monsterBulletPrefabs[i], poolSize);
+        }
+
     }
 
     public MonsterStateManager SpawnMonster()
@@ -29,7 +49,7 @@ public class MonsterManager : ScenesSingleton<MonsterManager>
 
         // 어떤 몬스터를 소환할지 랜덤으로 고르기
         int randomIndex = Random.Range(0, monsterPrefabs.Length);
-
+        print($"몬스터 프리팹 : {monsterPrefabs[randomIndex].GetType()}");
         // 선택된 프리팹을 기준으로 풀에서 몬스터를 꺼냄
         MonsterStateManager monster = PoolManager.Instance.GetPool(monsterPrefabs[randomIndex]);
 
