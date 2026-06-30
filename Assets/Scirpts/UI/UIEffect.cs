f12794cd64a47c9708d73d683a0908f05acafaec
hoy467 <hoyhoy4678@gmail.com>
2026-06-30 18:39:05 +0900

    assest:Add
    문 UI효과 추가
    히든 무기 추가

---
+++ Assets/Scirpts/UI/UIEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UIEffect : MonoBehaviour
+{
+    [Header("설정")]
+    [SerializeField] private float minScale = 0.01f;
+    [SerializeField] private float maxScale = 0.015f;
+    [SerializeField] private float speed = 2.0f;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        float scale = Mathf.Lerp(minScale, maxScale, (Mathf.Sin(Time.time * speed) + 1f) / 2f);
+
+        rectTransform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
