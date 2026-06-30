8e7458b06185f09d2ca91c4537e99c87acdba855
hoy467 <hoyhoy4678@gmail.com>
2026-06-30 18:39:05 +0900

    assest:Add
    문 UI효과 추가
    히든 무기 추가

--- Assets/Scirpts/Maps/NewRoom/Object/Chest.cs
+++ Assets/Scirpts/Maps/NewRoom/Object/Chest.cs
@@ -32,8 +32,6 @@ public class Chest : MonoBehaviour
         int randomValue = Random.Range(minCost, maxCost + 1);
 
         fixedCost = Mathf.Clamp(randomValue, minCost, maxCost);
-
-        Debug.Log($"확정된 비용: {fixedCost}");
     }
     private void Update()
     {
